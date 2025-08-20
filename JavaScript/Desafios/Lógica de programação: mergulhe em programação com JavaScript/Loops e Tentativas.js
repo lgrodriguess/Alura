bbd2e7de232ea81ac437873ac8587e707d4a0fbd
Luan Rodrigues <lrodriges2407@gmail.com>
2025-08-19 21:13:47 -0300

    Update Loops e Tentativas.js

--- JavaScript/Desafios/Lógica de programação: mergulhe em programação com JavaScript/Loops e Tentativas.js
+++ JavaScript/Desafios/Lógica de programação: mergulhe em programação com JavaScript/Loops e Tentativas.js
@@ -1,1 +1,15 @@
+// Corrigindo erro de loop infinito
 
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = 0
+
+while(contador < qtdNumeros){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+    contador++;
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
