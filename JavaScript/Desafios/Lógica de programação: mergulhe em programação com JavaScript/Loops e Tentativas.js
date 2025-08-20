31d2de2ab3d93e1e02667bf0f66e4ffc4549e209
Luan Rodrigues <lrodriges2407@gmail.com>
2025-08-19 21:20:15 -0300

    Update Loops e Tentativas.js

--- JavaScript/Desafios/Lógica de programação: mergulhe em programação com JavaScript/Loops e Tentativas.js
+++ JavaScript/Desafios/Lógica de programação: mergulhe em programação com JavaScript/Loops e Tentativas.js
@@ -14,3 +14,29 @@ let media = soma / qtdNumeros;
 
 console.log(media);
 //------------------------------------------------------------------------------
+
+//Desafios
+//1 - Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+let contador = 0;
+
+while(contador <= 10){
+    alert(contador);
+    contador++;
+}
+
+//2 - Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador = 10;
+
+while(contador >= 0){
+    alert(contador);
+    contador--;
+}
+
+//3 - Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+alert('Vamos realizar uma contagem regressiva.');
+let quantidade = prompt('Digite o numero de inicio!')
+
+while(quantidade >= 0){
+    alert(quantidade);
+    quantidade--;
+}
