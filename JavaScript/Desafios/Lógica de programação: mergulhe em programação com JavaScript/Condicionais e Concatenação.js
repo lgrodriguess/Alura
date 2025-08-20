3b8bdc3dedfb19e68b2d3fa6403bdfcca403d728
Luan Rodrigues <lrodriges2407@gmail.com>
2025-08-19 21:16:07 -0300

    Update Condicionais e Concatenação.js

--- JavaScript/Desafios/Lógica de programação: mergulhe em programação com JavaScript/Condicionais e Concatenação.js
+++ JavaScript/Desafios/Lógica de programação: mergulhe em programação com JavaScript/Condicionais e Concatenação.js
@@ -1,4 +1,4 @@
-
+//Desafios
 //1 - Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
 
 alert('Qual é o dia da semana?');
