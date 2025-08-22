78f9cec4837b2921b0cddfdfab68182c61856811
Luan Rodrigues <lrodriges2407@gmail.com>
2025-08-22 15:02:49 -0300

    Update Interagindo com HTML.js

--- JavaScript/Lógica de programação: explore funções e listas./Interagindo com HTML.js
+++ JavaScript/Lógica de programação: explore funções e listas./Interagindo com HTML.js
@@ -1,1 +1,7 @@
 //Desafio
+// 2 - Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+let texto = document.querySelector('h1');
+texto.innerHTML = 'Hora do Desafio!';
+
+// 3 - Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+
