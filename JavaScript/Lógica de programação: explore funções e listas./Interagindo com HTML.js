f752f7276270db74369a2bb34ba39f608caf3a1d
Luan Rodrigues <lrodriges2407@gmail.com>
2025-08-22 15:07:09 -0300

    Update Interagindo com HTML.js

--- JavaScript/Lógica de programação: explore funções e listas./Interagindo com HTML.js
+++ JavaScript/Lógica de programação: explore funções e listas./Interagindo com HTML.js
@@ -4,4 +4,6 @@ let texto = document.querySelector('h1');
 texto.innerHTML = 'Hora do Desafio!';
 
 // 3 - Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
-
+function verificarClique(){
+    console.log('O botão foi clicado!');
+}
